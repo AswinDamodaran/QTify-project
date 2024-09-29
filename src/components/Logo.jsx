99a9787c093580fbd14bf1694db80aa8160057e2
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-09-29 18:38:49 +0530

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import LogoImage from "../materials/qtify.svg";
 
 export default function Logo() {
-  return  <img src={LogoImage} alt="logo" width={67} style={{marginLeft:"20px",display:"block"}} />;
+  return  <img src={LogoImage} alt="logo" width={67} style={{marginLeft:"20px",display:"block",alignItems:"center"}} />;
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Button from "../Button/Button";
 
 function Navbar({ searchData }) {
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} style={{alignItems:"center"}}>
       <Logo  className={styles.logo} />
       <Search />
       <Button />
