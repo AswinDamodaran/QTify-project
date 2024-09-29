feb250e153abf3ce7bf4be2ae45ac80d3c8fa925
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-09-29 16:35:06 +0530

    fontagain

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import { Button } from "@mui/material";
 export default function button(){
     return (
         <div>
-            <Button className="btn" sx={{color:"#34C94B",fontFamily:"'Poppins', sans-serif"}} variant="contained">Give Feedback</Button>
+            <Button className="btn" sx={{color:"#34C94B",fontFamily:"Poppins"}} variant="contained">Give Feedback</Button>
         </div>
     )
 }
