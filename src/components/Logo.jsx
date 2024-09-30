37dd0f0378e5301c0c303cbc7fa97a10964d76ab
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-09-30 13:42:05 +0530

    he

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import LogoImage from "../materials/qtify.svg";
 
 export default function Logo() {
-  return  <img src={LogoImage} alt="logo" width={67} style={{marginLeft:"20px",display:"block",alignItems:"center"}} />;
+  return  <img src={LogoImage} alt="logo" height={"34px"} width={"67px"} style={{marginLeft:"20px",display:"block",alignItems:"center"}} />;
 }
